bab910f7517600f8c218d2e3ef1a1f8b6ed2d4f0
“sschadwick” <sschadwick@gmail.comssh-keygen -t rsa -C sschadwick@gmail.com>
2015-07-28 09:54:19 -0700

    added verb and noun post routes

--- app/script.js
+++ app/script.js
@@ -1,21 +1,24 @@
-'use-strict'
+'use strict'
 
 $(document).ready(function() {
 
-  $("#name").click(function() {
-    $.get("adjective", function(response) {
+  $('#name').click(function() {
+    $.get('adjective', function(response) {
       var adjective = response.word;
       $('#adjective').text(adjective);
     });
-    $.get("verb", function(response) {
+
+    $.get('verb', function(response) {
       var verb = response.word;
       $('#verb').text(verb);
     });
-    $.get("noun", function(response) {
+
+    $.get('noun', function(response) {
       var noun = response.word;
       $('#noun').text(noun);
     });
   });
+
   //make an event handler that, when the button is clicked,
   //sends a POST request to the server
   $('#submitWords').on('submit', function(e) { //e is event
@@ -25,11 +28,33 @@ $(document).ready(function() {
     var adjective = $('input[name=adjective]').val();
     var adjectivePost;
 
-    if(adjective) {
+    if (adjective) {
       adjectivePost = {word: adjective}; //object to JSON to send to backend
       $.post('adjective', adjectivePost, function(response) { //POST request to server
         var adjectiveRes = response.message;
         $('#adjectiveRes').text(adjectiveRes); //update html with response
+      });
+    }
+
+    var verb = $('input[name=verb]').val();
+    var verbPost;
+
+    if (verb) {
+      verbPost = {word: verb};
+      $.post('verb', verbPost, function(response) {
+        var verbRes = response.message;
+        $('#verbRes').text(verbRes);
+      });
+    }
+
+    var noun = $('input[name=noun]').val();
+    var nounPost;
+
+    if (noun) {
+      nounPost = {word: noun};
+      $.post('noun', nounPost, function(response) {
+        var nounRes = response.message;
+        $('#nounRes').text(nounRes);
       });
     }
   });
--- server.js
+++ server.js
@@ -14,8 +14,7 @@ var port = process.env.PORT || 3000;
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true})); //just a necessary thing
-app.use(express.static(__dirname + '/app/'));
-//lets app have access to specified directory
+app.use(express.static(__dirname + '/app/')); //lets app have access to specified directory
 
 //make an instance of that adjective object
 var adjective = new Adjective();
@@ -31,7 +30,6 @@ app.get('/adjective', function(req, res) {
 });
 
 app.post('/adjective', function(req, res) {
-  console.log(req.body); //debugging
   res.json(postRandomWord(req.body.word, adjective)); //jsonify and send back
 });
 
@@ -39,9 +37,17 @@ app.get('/verb', function(req, res) {
   res.json(getRandomWord(verb));
 });
 
-app.get('/noun', function(req, res){
+app.post('/verb', function(req, res) {
+  res.json(postRandomWord(req.body.word, verb));
+});
+
+app.get('/noun', function(req, res) {
   res.json(getRandomWord(noun));
-})
+});
+
+app.post('/noun', function(req, res) {
+  res.json(postRandomWord(req.body.word, noun));
+});
 
 app.listen(port, function() {
   console.log('server started on port ' + port);
