8e0a20776c412dc906cadc42d41526b1593fc96e
“sschadwick” <sschadwick@gmail.comssh-keygen -t rsa -C sschadwick@gmail.com>
2015-07-28 07:55:07 -0700

    added adj post route, modularized lib

--- app/script.js
+++ app/script.js
@@ -5,16 +5,32 @@ $(document).ready(function() {
   $("#name").click(function() {
     $.get("adjective", function(response) {
       var adjective = response.word;
-      $("#adjective").text(adjective);
+      $('#adjective').text(adjective);
     });
     $.get("verb", function(response) {
       var verb = response.word;
-      $("#verb").text(verb);
+      $('#verb').text(verb);
     });
     $.get("noun", function(response) {
       var noun = response.word;
-      $("#noun").text(noun);
+      $('#noun').text(noun);
     });
   });
+  //make an event handler that, when the button is clicked,
+  //sends a POST request to the server
+  $('#submitWords').on('submit', function(e) { //e is event
+    e.preventDefault(); //prevent page from reloading on submit
 
+    // get the text entered in the text box and save to a variable
+    var adjective = $('input[name=adjective]').val();
+    var adjectivePost;
+
+    if(adjective) {
+      adjectivePost = {word: adjective}; //object to JSON to send to backend
+      $.post('adjective', adjectivePost, function(response) { //POST request to server
+        var adjectiveRes = response.message;
+        $('#adjectiveRes').text(adjectiveRes); //update html with response
+      });
+    }
+  });
 });
